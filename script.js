f916d34228c713ecaba216823c490f6962b9de7a
Abdulquadri <olayanju.olaide@olivetsolutions>
2019-05-04 22:19:43 +0100

    uncomment script.js file

--- script.js
+++ script.js
@@ -1,84 +1,20 @@
-// var css = document.querySelector("h3");
-// var color1 = document.querySelector(".color1");
-// var color2 = document.querySelector(".color2");
-// var body = document.getElementById("gradient");
+ var css = document.querySelector("h3");
+ var color1 = document.querySelector(".color1");
+ var color2 = document.querySelector(".color2");
+ var body = document.getElementById("gradient");
 
-// function setGradient() {
-// 	body.style.background = 
-// 	"linear-gradient(to right, " 
-// 	+ color1.value 
-// 	+ ", " 
-// 	+ color2.value 
-// 	+ ")";
+ function setGradient() {
+ 	body.style.background = 
+ 	"linear-gradient(to right, " 
+ 	+ color1.value 
+ 	+ ", " 
+ 	+ color2.value 
+ 	+ ")";
 
-// 	css.textContent = body.style.background + ";";
-// }
+ 	css.textContent = body.style.background + ";";
+ 
 
-// color1.addEventListener("input", setGradient);
+ color1.addEventListener("input", setGradient);
 
-// color2.addEventListener("input", setGradient);
+ color2.addEventListener("input", setGradient);
 
-// function isUserValid(bool){
-// 	return bool;
-// }
-
-// var answer = isUserValid(true) ? "You May Access" : "Access Denied";
-
-// var automatedAnswer = "Your account # is" + (isUserValid(false)
-//  ? "1234" : "not available");
-
-// function moveCommand(direction){
-// 	var whatHappens;
-// 	switch (direction){
-// 		case 1:
-// 		whatHappens = "you encounter a monster";
-// 		break;
-// 		case 2:
-// 		whatHappens = "Find you way way out";
-// 		break;
-// 		default:
-// 		whatHappens = "You are wrong";
-// 	}
-// 	console.log(whatHappens);
-// }
-
-// function moveCommand(age){
-
-
-
-// var students;
-
-// switch (age){
-// 	case 16:
-// 	students = "You Can Get License";
-// 	break;
-
-// 	case 17:
-// 	students = "You are SuperReady";
-// 	break;
-
-// 	case 15: 
-// 	students = "You are too Young to drive";
-// 	break;
-
-// 	default:
-// 	students = "Wrong Info";
-
-// }
-// return students;
-
-// }
-
-// const player = "Essien";
-// let experience = "100";
-// let wizardLevel = false;
-
-// if (experience = 90){
-// 	let wizardLevel = true;
-// 	console.log('inside', wizardLevel);
-// }
-
-// console.log('outside', wizardLevel);
-
-
-const mutiply = (a, b) > 
